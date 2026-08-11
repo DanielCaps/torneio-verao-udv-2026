6eaca5b4b6775c2a8000dfbfc23e77d9c7be17ef
Daniel Neiva <47665667+DanielCaps@users.noreply.github.com>
2026-08-11 17:14:46 +0100

    v2 update

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 // Depois de publicares o Google Apps Script, cola aqui o URL do Web App.
 window.APP_CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbx8e_gQvK46Joyj84QGvvbq8h0KHw6agJgMPRd1-s2oXACk10oZwu_5I516mC5paEbs/exec"
+  API_URL: "https://script.google.com/macros/s/AKfycbz_8tLMYSSUl8MbW0hfyrnPy5WUy1W9Ap3b8_QdQLrYj_HtPn8wqO56fz_zkCgn1Pib/exec"
 };
